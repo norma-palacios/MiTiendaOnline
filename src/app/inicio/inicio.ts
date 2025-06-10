2726034500defc17402d1e3e70aaafd2ef90e934
norma-palacios <pr242879@alumno.udb.edu.sv>
2025-06-09 23:00:29 -0600

    agregar carousel

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductosService, Producto } from '../servicios/productos.service';
@@ -11,6 +11,14 @@ import { ProductosService, Producto } from '../servicios/productos.service';
   styleUrls: ['./inicio.css']
 })
 export class Inicio implements OnInit {
+
+  //carousel
+  indiceActual: number = 0;
+  imagenes: string[] = [
+    'assets/img/logo1.png',
+    'assets/img/logo2.png',
+    'assets/img/logo3.png'
+  ];
 
   textoBusqueda: string = '';
   categoriaSeleccionada: string = '';
@@ -32,12 +40,22 @@ export class Inicio implements OnInit {
     });
   }
 
+
+  siguiente(): void {
+    this.indiceActual = (this.indiceActual + 1) % this.imagenes.length;
+  }
+
+  anterior(): void {
+    this.indiceActual = (this.indiceActual - 1 + this.imagenes.length) % this.imagenes.length;
+  }
+
+
   toggleDropdown(): void {
     this.dropdownAbierto = !this.dropdownAbierto;
   }
 
   seleccionarCategoria(cat: string, event: MouseEvent): void {
-    event.stopImmediatePropagation();
+    //event.stopImmediatePropagation();
     this.categoriaSeleccionada = cat;
     this.aplicarFiltros();
     this.toggleDropdown();
